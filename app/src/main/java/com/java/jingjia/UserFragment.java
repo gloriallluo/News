0d8d5ba7b336a844d1dfe27088973ffd900a6787
gloriallluo <jingjia_luo@163.com>
2020-09-05 00:30:19 +0800

    add a simple user fragment interface

--- app/src/main/java/com/java/jingjia/UserFragment.java
+++ app/src/main/java/com/java/jingjia/UserFragment.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,10 +15,10 @@ import androidx.fragment.app.Fragment;
 public class UserFragment extends Fragment {
 
     private String TAG = "UserFragment";
+    private TextView userCollection, userDownloads, userHistory;
     private Activity mActivity;
 
     public UserFragment(Activity activity) {
-        Log.d(TAG, "UserFragment: new UserFragment");
         mActivity = activity;
     }
 
@@ -25,6 +26,35 @@ public class UserFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_user, container, false);
+        bindViews(view);
+        setListeners();
         return view;
+    }
+    
+    private void bindViews(View view) {
+        userCollection = view.findViewById(R.id.user_collection);
+        userDownloads = view.findViewById(R.id.user_downloads);
+        userHistory = view.findViewById(R.id.user_history);
+    }
+    
+    private void setListeners() {
+        userCollection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: click collection");
+            }
+        });
+        userDownloads.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: click downloads");
+            }
+        });
+        userHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: click history");
+            }
+        });
     }
 }
