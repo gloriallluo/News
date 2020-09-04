e3804a1f2aacb3649fb4098daa50a55e723d02df
xyd18 <xyd18@mails.tsinghua.edu.cn>
2020-09-05 00:38:13 +0800

    添加了简陋的从url获取json和SQLite版本的数据库（因为是Android Studio自带就先试了试），明天把数据库改成Room试试。 另外我在思考在app首页刷新不需要显示新闻内容，但是却要把新闻内容和题目一起先载入好像很耗时，为什么没有只包含题目等简短信息的接口呢。 我也还没有分清助教给的新闻列表接口和新闻事件接口有啥区别，感觉都包含了很多新闻正文，下面又有个新闻正文接口，不知道何时该用哪个呢？
    Signed-off-by: xyd18 <xyd18@mails.tsinghua.edu.cn>

---
+++ app/src/main/java/com/java/jingjia/request/HttpUtil.java
@@ -0,0 +1,160 @@
+package com.java.jingjia.request;
+
+import android.os.AsyncTask;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.SocketException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.concurrent.ExecutionException;
+
+public class HttpUtil {
+
+    private static HttpUtil INSTANCE = null;
+
+    private HttpUtil() {
+    }
+
+    public static HttpUtil getServerHttpResponse() {
+        if (INSTANCE == null) {
+            INSTANCE = new HttpUtil();
+        }
+        return INSTANCE;
+    }
+
+    public String getResponse(String oriUrl) {
+        try {
+            GetHttpResponseTask getHttpResponseTask = new GetHttpResponseTask();
+            String jsonString = getHttpResponseTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, oriUrl).get();
+            return jsonString;
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+//    public  String postResponse(String oriUrl, String data){
+//        try{
+//            PostHttpResponseTask postHttpResponseTask = new PostHttpResponseTask();
+//            return postHttpResponseTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR,oriUrl, data).get();
+//        }catch (ExecutionException e){
+//            e.printStackTrace();
+//        }catch (InterruptedException e){
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
+
+    private String getHttpResponse(String allConfigUrl) {
+        BufferedReader in = null;
+//        System.out.println("我被调用啦");
+        try {
+            URL url = new URL(allConfigUrl);
+            URLConnection connection = (URLConnection) url.openConnection();
+            connection.setRequestProperty("Charset", "UTF-8");
+            connection.setDoInput(true);
+            connection.setReadTimeout(2000);
+            connection.setConnectTimeout(2000);
+            connection.connect();
+
+            StringBuilder result = new StringBuilder();
+            in = new BufferedReader(new InputStreamReader(
+                    connection.getInputStream()));
+            String line;
+            while ((line = in.readLine()) != null) {
+                result.append(line);
+            }
+            in.close();
+            return result.toString();
+        } catch (SocketException e) {
+            return null;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    private class GetHttpResponseTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... allConfigUrl) {
+            return getHttpResponse(allConfigUrl[0]);
+        }
+    }
+
+
+//    private String postHttpResponse(String oriUrl, String data){
+//        try{
+//            URL url = new URL(oriUrl);
+//            URLConnection connection = (URLConnection) url.openConnection();
+//            connection.addRequestProperty("encoding", "UTF-8");//添加请求属性
+//            connection.setDoInput(true);//允许输入
+//            connection.setDoOutput(true);//允许输出
+////            connection.setRequestMethod("POST");//POST请求 要在获取输入输出流之前设置  否则报错
+//            connection.setConnectTimeout(500);
+//            connection.setReadTimeout(500);
+//            connection.connect();
+//
+//            //输出
+//            OutputStream os;
+//            os = connection.getOutputStream();
+//            OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");
+//            BufferedWriter bw = new BufferedWriter(osw);
+//            bw.write(data);
+//            bw.flush();
+//
+//            //输入
+//            InputStream in = connection.getInputStream();
+//            InputStreamReader isr = new InputStreamReader(in,"UTF-8");
+//            BufferedReader br = new BufferedReader(isr);
+//
+//            String line;
+//            StringBuilder sb = new StringBuilder();
+//            while((line = br.readLine()) != null)
+//            {
+//                sb.append(line);
+//            }
+//            bw.close();
+//            osw.close();
+//            os.close();
+//            br.close();
+//            isr.close();
+//            in.close();
+//
+//            return sb.toString();
+//        }catch (SocketException e){
+//            return null;
+//        }catch(MalformedURLException e){
+//            e.printStackTrace();
+//        }catch(IOException e){
+//            e.printStackTrace();
+//        } catch (MalformedURLException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
+
+//    public class PostHttpResponseTask extends AsyncTask<String, Void, String> {
+//        @Override
+//        protected String doInBackground(String... objects){
+//            return postHttpResponse(objects[0], objects[1]);
+//        }
+//    }
+}
