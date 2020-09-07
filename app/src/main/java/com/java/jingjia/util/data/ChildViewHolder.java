21c19f768a361bdea92d955a6fe82c00dc47dba8
xyd18 <xyd18@mails.tsinghua.edu.cn>
2020-09-07 16:14:23 +0800

    在DataManager里添加了很多有用的方法
    Signed-off-by: xyd18 <xyd18@mails.tsinghua.edu.cn>

--- app/src/main/java/com/java/jingjia/util/data/ChildViewHolder.java
+++ app/src/main/java/com/java/jingjia/util/data/ChildViewHolder.java
@@ -3,10 +3,6 @@ package com.java.jingjia.util.data;
 import android.content.Context;
 import android.view.View;
 
-import com.java.jingjia.DataBean;
-import com.java.jingjia.database.Data;
-import com.java.jingjia.util.data.BaseViewHolder;
-
 public class ChildViewHolder extends BaseViewHolder {
     private Context mContext;
 
